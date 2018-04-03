4635bfb3842d19ad42a7cb1e3e189576c0b8feeb
Chris Walter <walt50@gmail.com>
2018-04-02 20:32:56 -0400

    Initial Commit

--- Fifteen/ViewController.swift
+++ Fifteen/ViewController.swift
@@ -10,15 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    var blockWidth: CGFloat = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("screen width: \(width), screenHeight: \(height)")
+        blockWidth = height / 6.0
+        createGameBoard()
+        
+        
+    }
+    
+    func createGameBoard() {
+        // create a 4x4 grid of buttons
+        let center = CGPoint(x: width * 0.5, y: height * 0.5)
+        for row in 0...3 {
+            for column in 0...3 {
+                // to find the position move to the center, then shift 2 blocks back and up.
+                let x = Int(center.x) - (blockWidth + 4) + column * (blockWidth + 2)
+                let y = Int(center.y) - (blockWidth + 4) + row * (blockWidth + 2)
+                let button = UIButton(frame: CGRect(x: x, y: y, width: blockWidth, height: blockWidth))
+                button.backgroundColor = UIColor.green
+                button.layer.cornerRadius = 10.0
+                button.layer.borderColor = UIColor.darkGray.cgColor
+                button.layer.borderWidth = 2.0
+                button.setTitle("Test", for: UIControlState())
+                self.view.addSubview(button)
+                
+            }
+        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
