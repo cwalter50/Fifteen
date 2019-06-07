a26e85d0dc2d1452b4836f73a8c6f16c29f85d5f
cwalter50 <walt50@gmail.com>
2019-06-07 12:53:58 -0400

    Created Target for iOS. MenuVC is complete. Working on GameVC

---
+++ Slidearoo/GameVC.swift
@@ -0,0 +1,355 @@
+//
+//  GameVC.swift
+//  Slidearoo
+//
+//  Created by  on 6/7/19.
+//  Copyright © 2019 AssistStat. All rights reserved.
+//
+
+import UIKit
+import CloudKit
+
+
+class GameVC: UIViewController, PlayAgainDelegate {
+    
+    // MARK: Properties
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    //    var blockWidth: CGFloat = 0.0
+    
+    var scores: [Score] = []
+    
+    var gameSettings = GameSettings() // this carries rows, columns, shuffle, etc
+    
+    var board: Board = Board(rows: 4, columns: 4)
+    var savedBoard: Board?
+    var gameScore: Score? // this will be used to pass winning game to highScoresVC
+    
+    var gameWon = false // this is used to prevent the user from keep moving the board around with swipes after the game is won
+    
+    
+    var timer: Timer = Timer() // time is kept in the board class
+    
+    var timerLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        label.backgroundColor = UIColor.sunFlowerLight
+        label.font = UIFont(name: "Avenir", size: 50.0)
+        label.text = "0:00"
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    var movesLabel: UILabel = {
+        let label = UILabel(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        label.backgroundColor = UIColor.sunFlowerLight
+        label.font = UIFont(name: "Avenir", size: 50.0)
+        label.text = "Moves: 0"
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        return label
+    }()
+    
+    var howToButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        button.backgroundColor = UIColor.mintDark
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 50.0)
+        button.setTitle("How to Play", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(howToPlay), for: .primaryActionTriggered)
+        return button
+    }()
+    
+    var quitButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        button.backgroundColor = UIColor.grapefruitDark
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 50.0)
+        button.setTitle("Quit", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(quitGame), for: .primaryActionTriggered)
+        return button
+    }()
+    
+    var resetButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        button.backgroundColor = UIColor.grapefruitDark
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 50.0)
+        button.setTitle("Reset", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(resetBoard), for: .primaryActionTriggered)
+        return button
+    }()
+    
+    var pauseButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        button.backgroundColor = UIColor.sunFlowerDark
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 50.0)
+        button.setTitle("Pause", for: UIControl.State.normal)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 60.0)
+        button.addTarget(self, action: #selector(pauseGame), for: .primaryActionTriggered)
+        return button
+    }()
+    
+    var saveButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 300, height: 100))
+        button.backgroundColor = UIColor.mintDark
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 50.0)
+        button.setTitle("Save", for: UIControl.State.normal)
+        button.addTarget(self, action: #selector(saveGame), for: .primaryActionTriggered)
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //        view.backgroundColor = UIColor.white
+        setUpSwipeGestures()
+        createOrLoadGameBoard()
+        createLabelsAndButtons()
+        updateMovesLabel()
+        
+        // start timer
+        startTimer()
+        
+        
+    }
+    
+    func setUpSwipeGestures() {
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeRight.direction = .right
+        self.view.addGestureRecognizer(swipeRight)
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeLeft.direction = .left
+        self.view.addGestureRecognizer(swipeLeft)
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeUp.direction = .up
+        self.view.addGestureRecognizer(swipeUp)
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeDown.direction = .down
+        self.view.addGestureRecognizer(swipeDown)
+        
+    }
+    @objc func swiped(sender: UISwipeGestureRecognizer) {
+        // add logic to move pieces if its a valid direction. Also if the game is not paused or won
+        if gameWon == false && gamePaused == false {
+            board.moveDirection(direction: sender.direction)
+            updateMovesLabel()
+            // check if board is solved
+            if board.isSolved() {
+                print("board solved!!!")
+                timer.invalidate()
+                solvedBoardAlert(moves: board.moves)
+            }
+        }
+    }
+    
+    func startTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { (timer) in
+            self.board.time += 1
+            let min = self.board.time / 60
+            let sec = self.board.time % 60
+            let timeText = String(format:"%i:%02i",min, sec)
+            self.timerLabel.text = timeText
+        })
+    }
+    
+    // this method is called whenever the focused item changes.. Only needed for tvOS
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        let tile = context.nextFocusedView
+        tile?.layer.shadowOffset = CGSize(width: 0, height: 10)
+        tile?.layer.shadowOpacity = 0.6
+        tile?.layer.shadowRadius = 15
+        tile?.layer.shadowColor = UIColor.black.cgColor
+        context.previouslyFocusedView?.layer.shadowOpacity = 0
+    }
+    
+    func createLabelsAndButtons() {
+        
+        
+        
+        self.view.addSubview(timerLabel)
+        timerLabel.center = CGPoint(x: width * 0.43, y: height * 0.07)
+        
+        self.view.addSubview(movesLabel)
+        movesLabel.center = CGPoint(x: width * 0.6, y: height * 0.07)
+        
+        let topHorizontalStack = UIStackView(arrangedSubviews: [timerLabel, movesLabel])
+        topHorizontalStack.axis = .horizontal
+        topHorizontalStack.distribution = .fillEqually
+        topHorizontalStack.alignment = .fill
+        topHorizontalStack.spacing = 20
+        topHorizontalStack.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(topHorizontalStack)
+        
+        
+        
+        
+        
+        
+        self.view.addSubview(howToButton)
+        howToButton.center = CGPoint(x: width * 0.1, y: height * 0.94)
+        self.view.addSubview(quitButton)
+        quitButton.center = CGPoint(x: width * 0.3, y: height * 0.94)
+        self.view.addSubview(resetButton)
+        resetButton.center = CGPoint(x: width * 0.5, y: height * 0.94)
+        self.view.addSubview(pauseButton)
+        pauseButton.center = CGPoint(x: width * 0.7, y: height * 0.94)
+        self.view.addSubview(saveButton)
+        saveButton.center = CGPoint(x: width * 0.9, y: height * 0.94)
+        
+        let bottomHorizontalStack = UIStackView(arrangedSubviews: [howToButton, quitButton, resetButton, pauseButton, saveButton])
+        bottomHorizontalStack.axis = .horizontal
+        bottomHorizontalStack.distribution = .fillEqually
+    }
+    func updateMovesLabel() {
+        movesLabel.text = "Moves: \(board.moves)"
+    }
+    
+    // this will either load the saved board, or create a new board
+    func createOrLoadGameBoard() {
+        // load saved Board if it exists, otherwise create a new board
+        if let theBoard = savedBoard {
+            board = theBoard
+            // update timerLabel to fix the 1 second glitch
+            let min = self.board.time / 60
+            let sec = self.board.time % 60
+            let timeText = String(format:"%i:%02i",min, sec)
+            self.timerLabel.text = timeText
+        } else {
+            board = Board(rows: gameSettings.rows, columns: gameSettings.columns)
+            // figure out a setting to shuffle board for easy/ medium, or hard
+            board.shuffle(numberOfMoves: gameSettings.shuffleCount)
+            
+        }
+        self.view.addSubview(board.backgroundView)
+        
+        
+    }
+    
+    @objc func howToPlay() {
+        print("howToPlayTapped")
+        performSegue(withIdentifier: "HowToSegue", sender: self)
+    }
+    @objc func quitGame() {
+        print("quitGame tapped")
+        navigationController?.popViewController(animated: true)
+    }
+    @objc func saveGame(sender: UIButton) {
+        print("saveGame tapped")
+        // save board to user defaults and dismiss
+        let userDefaults = UserDefaults.standard
+        let encodedData: Data = NSKeyedArchiver.archivedData(withRootObject: board)
+        
+        userDefaults.set(encodedData, forKey: "savedBoard")
+        userDefaults.synchronize() // consider removing????
+        //        let defaults = UserDefaults.standard
+        //        defaults.set(board, forKey: "savedGame")
+        
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    @objc func resetBoard(sender: UIButton) {
+        // reset Moves label to 0, timer label to 0, solve puzzle, then shuffle Again
+        board.moves = 0
+        timer.invalidate()
+        self.board.time = 0
+        startTimer()
+        updateMovesLabel()
+        board.setBoardToInitialState()
+        
+    }
+    
+    var gamePaused = false
+    @objc func pauseGame(sender: UIButton) {
+        gamePaused = !gamePaused
+        if gamePaused {
+            // stop timer
+            timer.invalidate()
+            pauseButton.setTitle("Resume", for: .normal)
+            // make all numbers disapear
+            for tile in board.tiles {
+                tile.nameLabel.text = "#"
+            }
+        } else {
+            // DO NOT reset time
+            startTimer()
+            pauseButton.setTitle("Pause", for: .normal)
+            for tile in board.tiles {
+                tile.setTileTitle()
+            }
+            
+        }
+    }
+    
+    
+    func saveScoreInCloudKit(name: String) {
+        let newScore = Score(name: name, moves: board.moves, time: board.time, difficultyLevel: gameSettings.difficultyLevel)
+        newScore.delegate = self // set the delegate so that we can alert this view when cloud data is saved or if there is an error
+        newScore.saveToCloudkit() // I created a method to save to cloudkit within the class Score
+        //        scores.append(newScore)
+    }
+    
+    func solvedBoardAlert(moves: Int) {
+        let alert = UIAlertController(title: "You won in \(moves) moves!!!", message: "Enter your name below.", preferredStyle: .alert)
+        
+        alert.addTextField {textField in
+            textField.placeholder = "Enter Name Here"
+            textField.autocapitalizationType = .words
+            
+        }
+        let saveAction = UIAlertAction(title: "Save", style: .default, handler: {action in
+            
+            let nameTF = alert.textFields![0] // force unwrap because we know it's there
+            let name = nameTF.text ?? "Hulk"
+            // uncomment after testing
+            self.gameScore = Score(name: name, moves: self.board.moves, time: self.board.time, difficultyLevel: self.gameSettings.difficultyLevel)
+            //            self.performSegue(withIdentifier: "HighScoresSegue", sender: self)
+            self.saveScoreInCloudKit(name: name)
+            
+        })
+        let quitAction = UIAlertAction(title: "Quit", style: .cancel, handler: {
+            action in
+            self.navigationController?.popViewController(animated: true)
+        })
+        alert.addAction(saveAction)
+        alert.addAction(quitAction)
+        //        alert.addAction(noAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // this is called from highScoresVC if the user taps Play Again.
+    func playNewGame() {
+        // reload the game and playAgain
+        board.shuffle(numberOfMoves: gameSettings.shuffleCount)
+        board.moves = 0
+        timer.invalidate()
+        self.board.time = 0
+        startTimer()
+        updateMovesLabel()
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "HighScoresSegue" {
+            let destVC = segue.destination as! HighScoresViewController
+            // pass data here
+            destVC.gameScore = self.gameScore
+            destVC.delegate = self
+            //            destVC.scores = scores
+        }
+    }
+}
+
+extension GameVC: newScoreAddedDelegate {
+    // this function will get called after theScore is saved in cloudkit
+    func showHighScores(score: Score) {
+        self.gameScore = score
+        self.performSegue(withIdentifier: "HighScoresSegue", sender: self)
+        print("new Score has been added with difficulty level \(gameScore!.difficultyLevel)")
+    }
+    // this will be called if there is an error in saving the score to cloudkit
+    func errorAlert(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let yesAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(yesAction)
+        present(alert, animated: true, completion: nil)
+    }
+}
---
+++ Slidearoo/MenuVC.swift
@@ -0,0 +1,202 @@
+//
+//  MenuVC.swift
+//  Slidearoo
+//
+//  Created by  on 6/7/19.
+//  Copyright © 2019 AssistStat. All rights reserved.
+//
+
+import UIKit
+
+class MenuVC: UIViewController, CustomGameDelegate {
+    
+    // MARK: Properties
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    var gameSettings: GameSettings = GameSettings() // use default initializer for gameSettings which is 4 x 4 medium
+    
+    
+    var stackView = UIStackView()
+    var quickGameButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 400, height: 100))
+        button.backgroundColor = UIColor.mintDark
+        button.setTitle("Quick Game", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 60)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.addTarget(self, action: #selector(quickGame), for: .primaryActionTriggered)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var customGameButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 400, height: 100))
+        button.backgroundColor = UIColor.sunFlowerDark
+        button.setTitle("Custom Game", for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.addTarget(self, action: #selector(customGame), for: .primaryActionTriggered)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 60.0)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var resumeGameButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 400, height: 100))
+        button.backgroundColor = UIColor.aquaDark
+        button.setTitle("Resume Game", for: .normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.addTarget(self, action: #selector(resumeGame), for: .primaryActionTriggered)
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 60.0)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    var howToButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 100, y: 0, width: 400, height: 100))
+        button.backgroundColor = UIColor.grapefruitDark
+        button.titleLabel?.font = UIFont(name: "Avenir", size: 60.0)
+        button.setTitle("How to Play", for: UIControl.State.normal)
+        button.titleLabel?.adjustsFontSizeToFitWidth = true
+        button.addTarget(self, action: #selector(howToPlay), for: .primaryActionTriggered)
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        createLabelsAndButtons()
+        
+        if #available(tvOS 9.0,*)
+        {
+            print("Code that executes only on tvOS 9.0 or later.")
+        }
+        
+        if #available(iOS 10.0,*)
+        {
+            print("Code that executes only on iOS 10.0 or later.")
+        }
+        
+        
+    }
+
+    
+    // this method is called whenever the focused item changes.. Not necessary for iOS.. only tvOS
+    override func didUpdateFocus(in context: UIFocusUpdateContext, with coordinator: UIFocusAnimationCoordinator) {
+        super.didUpdateFocus(in: context, with: coordinator)
+        let button = context.nextFocusedView
+        button?.layer.shadowOffset = CGSize(width: 0, height: 10)
+        button?.layer.shadowOpacity = 0.6
+        button?.layer.shadowRadius = 15
+        button?.layer.shadowColor = UIColor.black.cgColor
+        context.previouslyFocusedView?.layer.shadowOpacity = 0
+    }
+    
+    func createLabelsAndButtons() {
+        //        self.view.backgroundColor = UIColor.white
+        self.view.addSubview(quickGameButton)
+        self.view.addSubview(customGameButton)
+        self.view.addSubview(resumeGameButton)
+        self.view.addSubview(howToButton)
+        
+        stackView = UIStackView(arrangedSubviews: [quickGameButton, customGameButton, resumeGameButton, howToButton])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(stackView)
+        
+        // add Constraints
+        stackView.heightAnchor.constraint(lessThanOrEqualToConstant: UIScreen.main.bounds.height/2).isActive = true
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(lessThanOrEqualToConstant: UIScreen.main.bounds.width*2/3).isActive = true
+//        stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
+//        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+//        stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40).isActive = true
+//        stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40).isActive = true
+        
+        
+    }
+    
+    @objc func customGame() {
+        performSegue(withIdentifier: "customGameSettingsSegue", sender: self)
+    }
+    
+    @objc func quickGame() {
+        performSegue(withIdentifier: "quickGameSegue", sender: self)
+    }
+    
+    @objc func howToPlay() {
+        performSegue(withIdentifier: "HowToSegue", sender: self)
+    }
+    
+    var savedBoard: Board?
+    
+    @objc func resumeGame() {
+        // load savedBoard if it exists.
+        let decodedData = UserDefaults.standard.object(forKey: "savedBoard") as? Data
+        
+        if decodedData != nil {
+            savedBoard = NSKeyedUnarchiver.unarchiveObject(with: decodedData!) as? Board
+        }
+        
+        if savedBoard != nil {
+            // pass savedBoard with segue
+            
+            performSegue(withIdentifier: "resumeGameSegue", sender: self)
+        } else {
+            // display error that says that we could not find a saved game.
+            let alert = UIAlertController(title: "We did not find a saved game.", message: "Would you like to play a new game?", preferredStyle: .alert)
+            let yesQuick = UIAlertAction(title: "Yes, Play Quick Game", style: .default, handler: {action in
+                self.quickGame()
+            })
+            let yesCustom = UIAlertAction(title: "Yes, Play Custom Game", style: .default, handler: {action in
+                self.customGame()
+            })
+            let no = UIAlertAction(title: "No", style: .default, handler: nil)
+            alert.addAction(yesQuick)
+            alert.addAction(yesCustom)
+            alert.addAction(no)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+    }
+    
+    func playGame(newGameSettings: GameSettings) {
+        self.gameSettings = newGameSettings
+        performSegue(withIdentifier: "playCustomGame", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "quickGameSegue" {
+            let destVC = segue.destination as! GameViewController
+            // pass in some settings like shuffle count and board size etc.
+            destVC.gameSettings = self.gameSettings
+        }
+        if segue.identifier == "customGameSettingsSegue" {
+            let destVC = segue.destination as! CustomGameViewController
+            // pass stuff here
+            destVC.delegate = self
+            destVC.gameSettings = self.gameSettings
+        }
+        if segue.identifier == "playCustomGame" {
+            let destVC = segue.destination as! GameViewController
+            // pass stuff here
+            destVC.gameSettings = self.gameSettings
+        }
+        
+        if segue.identifier == "resumeGameSegue"{
+            let destVC = segue.destination as! GameViewController
+            // pass stuff here
+            destVC.gameSettings = self.gameSettings
+            destVC.savedBoard = savedBoard
+            // figure out how to save current game and pass game to continue.
+        }
+    }
+    
+    
+}
